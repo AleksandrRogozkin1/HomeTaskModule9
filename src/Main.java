6ea7155499a9b63971dd42175f5cd0925914402a
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-02-17 18:42:48 +0200

    last commit

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,30 @@ public class Main {
         myArrayList.add(100);
         myArrayList.add(200);
         myArrayList.add(300);
-        System.out.println("myArrayList size"+myArrayList.size());
+        System.out.println("myArrayList size="+myArrayList.size());
         System.out.println(myArrayList.toString());
         myArrayList.remove(1);
         System.out.println(myArrayList.toString());
         myArrayList.clear();
         System.out.println(myArrayList);
         System.out.println(myArrayList.size());
+
+        System.out.println("\n\n\n\nLinked list");
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add("Madrid");
+        myLinkedList.add("Paris");
+        myLinkedList.add("Amsterdam");
+
+        System.out.println("myLinkedList = " + myLinkedList.toString());
+        System.out.println(myLinkedList.get(2));
+
+        myLinkedList.remove(1);
+        System.out.println("myLinkedList = " + myLinkedList.toString());
+
+        myLinkedList.clear();
+        System.out.println("myLinkedList = " + myLinkedList.toString());
+        myLinkedList.add("Kyiv");
+        System.out.println("myLinkedList = " + myLinkedList.toString());
 
 
     }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,67 +1,76 @@
-
-
 import java.util.Objects;
 import java.util.StringJoiner;
-public class MyLinkedList <E> {
-    private int size = 0;
+
+public class MyLinkedList<E> {
+    private int size;
+
     private Node<E> firstNode;
     private Node<E> lastNode;
-    private static class Node<E>{
-        private E item;
-        private MyLinkedList.Node<E> next;
-        private MyLinkedList.Node<E> prev;
-        Node(){
-
-        }
-        Node(MyLinkedList.Node<E> prevElement, E element, MyLinkedList.Node<E> nextElement) {
-            this.item = element;
-            this.next = nextElement;
-            this.prev = prevElement;
-        }
 
 
-        public E getItem() {
-            return item;
+
+    private Node<E> getNodeByIndex(int index){
+        int counter = 0;
+        Node<E> node = firstNode;
+
+        while (node!=null){
+            if(counter==index){
+                return node;
+            }
+            counter++;
+            node = node.getNextElement();
         }
 
-        public void setItem(E item) {
-            this.item = item;
-        }
-
-        public Node<E> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<E> next) {
-            this.next = next;
-        }
-
-        public Node<E> getPrev() {
-            return prev;
-        }
-
-        public void setPrev(Node<E> prev) {
-            this.prev = prev;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" +"item=" + item +'}';
-        }
+        return new Node<>();
     }
-    public void addLast(E value) {
+
+    private void setNullFirstLastNodes(){
+        firstNode = null;
+        lastNode = null;
+        size = 0;
+    }
+
+    public void add(E value){
         Node<E> node = new Node<>(lastNode, value, null);
 
         if(lastNode==null){
             firstNode = node;
         } else {
-            lastNode.setNext(node);
+            lastNode.setNextElement(node);
         }
 
         lastNode = node;
         size=  size + 1;
+    }
+
+    public void remove(int index){
+
+        if (size == 0){
+            return;
+        }
+
+        if (size==1){
+            setNullFirstLastNodes();
+            return;
+        }
+
+        Node<E> node = getNodeByIndex(index);
+
+        if(node.equals(firstNode)){
+            node.getNextElement().setPrevElement(null);
+            firstNode = node.getNextElement();
+        } else {
+            node.getPrevElement().setNextElement(node.getNextElement());
+        }
+
+        if (node.getNextElement()==null){
+            lastNode = node.getPrevElement();
+        }
+
+        size--;
 
     }
+
     public void clear(){
         if (size==0){
             return;
@@ -69,12 +78,86 @@ public class MyLinkedList <E> {
 
         Node<E> node = firstNode;
         while (node!=null){
-            node.setItem(null);
-            node.setPrev(null);
-            node = node.getNext();
+            node.setElement(null);
+            node.setPrevElement(null);
+            node = node.getNextElement();
         }
-        firstNode = lastNode = null;
-        size = 0;
+        setNullFirstLastNodes();
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public E get(int index){
+        Objects.checkIndex(index, size);
+        Node<E> node = getNodeByIndex(index);
+        return node.getElement();
+    }
+
+    @Override
+    public String toString() {
+        if (size==0){
+            return "{}";
+        }
+
+        StringJoiner res = new StringJoiner(",");
+        res.add(firstNode.toString());
+        Node<E> node = firstNode.getNextElement();
+        while (node!=null){
+            res.add(node.toString());
+            node =  node.getNextElement();
+        }
+
+        return "{"+res.toString()+"}";
+    }
+
+
+    private static class Node<E> {
+        private E element;
+        private MyLinkedList.Node<E> nextElement;
+        private MyLinkedList.Node<E> prevElement;
+
+        Node(){
+
+        }
+
+        Node(MyLinkedList.Node<E> prevElement, E element, MyLinkedList.Node<E> nextElement) {
+            this.element = element;
+            this.nextElement = nextElement;
+            this.prevElement = prevElement;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getNextElement() {
+            return nextElement;
+        }
+
+        public void setNextElement(Node<E> nextElement) {
+            this.nextElement = nextElement;
+        }
+
+        public Node<E> getPrevElement() {
+            return prevElement;
+        }
+
+        public void setPrevElement(Node<E> prevElement) {
+            this.prevElement = prevElement;
+        }
+
+        @Override
+        public String toString() {
+            return element.toString();
+        }
+
+
     }
 
 }
