8460084ada89513b8c6c7787d0140a6179d162ef
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-02-17 15:36:48 +0200

    last commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         MyArrayList<Integer> myArrayList = new MyArrayList<>();
-        myArrayList.add(1);
-        myArrayList.add(2);
-        myArrayList.add(3);
-
+        myArrayList.add(100);
+        myArrayList.add(200);
+        myArrayList.add(300);
+        System.out.println("myArrayList size"+myArrayList.size());
         System.out.println(myArrayList.toString());
-
-        System.out.println("------");
         myArrayList.remove(1);
         System.out.println(myArrayList.toString());
         myArrayList.clear();
